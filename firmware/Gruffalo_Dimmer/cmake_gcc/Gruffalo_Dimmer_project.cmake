01d15a325342abfdf5d0c49cb8ace3a898ed323a
Joel Santos <mundodisco8@gmail.com>
2026-02-03 12:01:12 +0000

    Fixed Generators in VSCode

---
+++ firmware/Gruffalo_Dimmer/cmake_gcc/Gruffalo_Dimmer_project.cmake
@@ -0,0 +1,22 @@
+#eyJzdGF0ZSI6eyJidWlsZFByZXNldHMiOnsiZGVmYXVsdF9jb25maWciOnsibmFtZSI6ImRlZmF1bHRfY29uZmlnIiwiY29uZmlndXJhdGlvbiI6ImJhc2UiLCJkZWZpbml0aW9ucyI6W10sInJlbW92ZWRGbGFncyI6eyJDIjpbIi1PcyIsIi1nIl0sIkNYWCI6W10sIkFTTSI6W119LCJhZGRpdGlvbmFsRmxhZ3MiOnsiQyI6WyItZ2dkYjMiLCItT2ciXSwiQ1hYIjpbXSwiQVNNIjpbXX19fSwiY3VycmVudENvbmZJbmRleCI6MH0sImZvbGRlcnMiOlsiaW5jIiwic3JjIl0sImZpbGVzIjpbXX0=
+include_directories(
+	"../inc"
+	"../src"
+)
+
+target_sources(Gruffalo_Dimmer PRIVATE
+	"../sl_gatt_service_device_information_override.c"
+	"../src/buttonActions.c"
+	"../src/buttons.c"
+	"../src/debounce.c"
+	"../src/gpio_HW.c"
+	"../src/interrupt_HW.c"
+	"../src/PWMControl.c"
+	"../src/sleepyTimers_HW.c"
+	"../src/timer_HW.c"
+)
+
+target_compile_options(slc PRIVATE
+	"$<$<AND:$<CONFIG:base>,$<COMPILE_LANGUAGE:C>>:-ggdb3>"
+	"$<$<AND:$<CONFIG:base>,$<COMPILE_LANGUAGE:C>>:-Og>"
+)
